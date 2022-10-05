c1af8016642917c6fe417799b204a496b57ad35e
Вован <wowa-puto@mail.ru>
2022-10-06 01:22:02 +0300

    chisla

--- chislo.py
+++ chislo.py
@@ -1,17 +1,19 @@
 chislo1 = 1
 chislo2 = 100
 print(chislo1**chislo2) #**
-chislo1=2
-chislo2=16
-print(chislo1**chislo2)
-chislo1=2
-chislo2=1000
-print(chislo1-chislo2)
-print(chislo1+chislo1*chislo1)
+chislo3=2
+chislo4=16
+print(chislo3**chislo4)
+chislo5=2
+chislo6=1000
+print(chislo5-chislo6)
+print(chislo5+chislo5*chislo5)
 import math
-chislo1=144
-print(math.sqrt(chislo1))
-chislo1=66
-chislo2=x
-chislo3=11
-print(chislo1-int(chislo2)=chislo3)
+chislo7=144
+print(math.sqrt(chislo7))
+chislo8=66
+chislo9=11
+print(chislo8-chislo9)
+chislo10=8
+chislo11=16
+print((chislo11-chislo10)**2)
